69c023a513439ef83cec4572e6653ad6377fdd6c
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-10 23:10:17 +0700

    feat: add mail service

--- Services/MailService.cs
+++ Services/MailService.cs
@@ -1,6 +1,26 @@
 ﻿namespace Ticket_Booking.Services
 {
-    public class MailService
+    public static class MailService
     {
+        
+        private static readonly IConfiguration _configuration;
+        public static void SendEmail(string toEmail, string subject, string body)
+        {
+            var smtpClient = new System.Net.Mail.SmtpClient(_configuration["Smtp:Host"])
+            {
+                Port = int.Parse(_configuration["Smtp:Port"]),
+                Credentials = new System.Net.NetworkCredential(_configuration["Smtp:Username"], _configuration["Smtp:Password"]),
+                EnableSsl = true,
+            };
+            var mailMessage = new System.Net.Mail.MailMessage
+            {
+                From = new System.Net.Mail.MailAddress(_configuration["Smtp:FromEmail"]),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(toEmail);
+            smtpClient.Send(mailMessage);
+        }
     }
 }
