913bc840a760eb1b51779410893d1162db87ce22
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-10 23:35:14 +0700

    feat: add OTP helper

---
+++ Helpers/OTP.cs
@@ -0,0 +1,16 @@
+﻿namespace Ticket_Booking.Helpers
+{
+    public static class OTP
+    {
+        private static string _currentOTP = string.Empty;
+        public static string GenerateOTP()
+        {
+            _currentOTP = Random.Shared.Next(100000, 999999).ToString();
+            return _currentOTP;
+        }
+        public static bool ValidateOTP(string otp)
+        {
+            return otp == _currentOTP;
+        }
+    }
+}
