45e310f9aa14f86307f58b66f6edf7727a90ed1a
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-12 00:39:17 +0700

    chore: minor change

--- Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ticket_Booking.Models.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
