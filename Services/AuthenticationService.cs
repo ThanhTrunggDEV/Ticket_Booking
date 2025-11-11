db38481603e49ebab2db83b4e399cab4384facbf
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-12 00:15:38 +0700

    feat: add authentication service

---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿namespace Ticket_Booking.Services
+{
+    public static class AuthenticationService
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = System.Security.Cryptography.SHA256.Create())
+            {
+                var bytes = System.Text.Encoding.UTF8.GetBytes(password);
+                var hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+        public static bool VerifyPassword(string enteredPassword, string storedHashedPassword)
+        {
+            var hashedEnteredPassword = HashPassword(enteredPassword);
+            return hashedEnteredPassword == storedHashedPassword;
+        }
+    }
+}
