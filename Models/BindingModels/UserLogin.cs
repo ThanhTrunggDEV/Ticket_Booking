b12132cc85738eef4689e675fcc088cf6d59310e
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-18 21:35:30 +0700

    feat: add binding model for login

---
+++ Models/BindingModels/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace Ticket_Booking.Models.BindingModels
+{
+    public class UserLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
