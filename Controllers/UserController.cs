b990f0a0b6c8f90cfd62381e1e572c7f36188056
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-20 20:25:46 +0700

    chore: add basic logic and design for user dashboard

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Ticket_Booking.Interfaces;
+using Ticket_Booking.Models.DomainModels;
 
 namespace Ticket_Booking.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly IRepository<User> _userRepository;
+
+        public UserController(IRepository<User> userRepository)
         {
-            return View();
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var user = await _userRepository.GetByIdAsync(userId.Value);
+            if (user == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            return View(user);
         }
     }
 }
