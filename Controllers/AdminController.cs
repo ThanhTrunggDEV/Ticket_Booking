c8679aabbed012b8664e2702878e4f1af0cc0bf2
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-20 12:18:02 +0700

    chore: init the admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticket_Booking.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
