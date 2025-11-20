58c8bf26937440fd38bfef64c96061ecb0a98d67
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-20 11:53:49 +0700

    chore: change OTP to stateless

--- Helpers/OTP.cs
+++ Helpers/OTP.cs
@@ -2,15 +2,9 @@
 {
     public static class OTP
     {
-        private static string _currentOTP = string.Empty;
         public static string GenerateOTP()
         {
-            _currentOTP = Random.Shared.Next(100000, 999999).ToString();
-            return _currentOTP;
-        }
-        public static bool ValidateOTP(string otp)
-        {
-            return otp == _currentOTP;
+            return Random.Shared.Next(100000, 999999).ToString();
         }
     }
 }
