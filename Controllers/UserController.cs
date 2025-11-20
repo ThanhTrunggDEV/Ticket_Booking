d84a545069fb5c5bbd9d1188f4ce0d369e2bfcd9
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-20 12:18:46 +0700

    chore: init the user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticket_Booking.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
