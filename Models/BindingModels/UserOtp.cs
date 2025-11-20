204cf18ad4eb9714b8c34479b81ec5be2f8a0f0b
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-20 11:58:16 +0700

    chore: add User Otp Model

---
+++ Models/BindingModels/UserOtp.cs
@@ -0,0 +1,15 @@
+﻿namespace Ticket_Booking.Models.BindingModels
+{
+    public class UserOtp
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string? Phone { get; set; }
+    }
+    
+    public class UserOtpModel : UserOtp
+    {
+        string Otp { get; set; }
+    }
+}
