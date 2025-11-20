9b9fa5683c93d64f313df9255c2b78f96ccc1885
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-20 12:18:16 +0700

    chore: init the partner controller

---
+++ Controllers/PartnerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticket_Booking.Controllers
+{
+    public class PartnerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
