afddd4eddcb631c0004ba4636d6dd003cbb20117
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-22 23:03:11 +0700

    add partner view model

---
+++ Models/ViewModels/PartnerDashboardViewModel.cs
@@ -0,0 +1,14 @@
+using Ticket_Booking.Models.DomainModels;
+
+namespace Ticket_Booking.Models.ViewModels
+{
+    public class PartnerDashboardViewModel
+    {
+        public int TotalTrips { get; set; }
+        public int TotalBookings { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public double AverageRating { get; set; }
+        public List<Trip> RecentTrips { get; set; } = new List<Trip>();
+        public List<Ticket> RecentBookings { get; set; } = new List<Ticket>();
+    }
+}
