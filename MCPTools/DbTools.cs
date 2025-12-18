0fcdecf60579ff5d9d5c8479ff88dc87ce0358c4
Nguyễn Thành Trung <trungff07@gmail.com>
2025-12-18 19:55:00 +0700

    feat: add MCP tools for LLM

---
+++ MCPTools/DbTools.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using ModelContextProtocol.Server;
+using Ticket_Booking.Data;
+using Ticket_Booking.Models.DomainModels;
+
+namespace Ticket_Booking.MCPTools
+{
+    [McpServerToolType]
+    public static class DbTools
+    {
+        private readonly static AppDbContext _context = new AppDbContext();
+        [McpServerTool, Description("Get all trips from the database")]
+        public static List<Trip> GetAllTrips() => _context.Trips.ToList();
+    }
+}
