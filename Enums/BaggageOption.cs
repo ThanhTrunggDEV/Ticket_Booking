862a086c7fb984d2b4070ef9f8ad3103a33beaff
Nguyễn Thành Trung <trungff07@gmail.com>
2025-12-23 13:56:57 +0700

    feat: add meal and baggage enums

---
+++ Enums/BaggageOption.cs
@@ -0,0 +1,11 @@
+namespace Ticket_Booking.Enums
+{
+    public enum BaggageOption
+    {
+        None = 0,
+        Kg15 = 15,
+        Kg20 = 20,
+        Kg30 = 30
+    }
+}
+
---
+++ Enums/MealOption.cs
@@ -0,0 +1,11 @@
+namespace Ticket_Booking.Enums
+{
+    public enum MealOption
+    {
+        None = 0,
+        Standard = 1,
+        Vegetarian = 2,
+        Special = 3
+    }
+}
+
